85d33e9c72aeab497eb9ffb0812f2d68031a37b2
Welen1911 <welenalmeida2210@gmail.com>
2024-08-17 20:46:19 -0300

    Componente de card para criar ou atualizar automovel

---
+++ src/components/cards/AutomobileCard.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import DefaultCard from '@/components/Forms/DefaultCard.vue';
+import InputGroup from '../Forms/InputGroup.vue';
+
+const props = defineProps({
+    title: {
+        type: String,
+        default: 'Card'
+    },
+    automobile: {
+        type: Object,
+        required: true
+    }
+});
+
+const emits = defineEmits(['onClick:submit']);
+
+const submit = () => emits('onClick:submit');
+
+</script>
+
+<template>
+    <DefaultCard :card-title="props.title">
+        <form @submit.prevent="submit" action="#">
+            <div class="p-6.5">
+                <div class="mb-4.5 flex flex-col gap-6 xl:flex-row">
+                    <InputGroup label="Ano" type="number" placeholder="2002" v-model="automobile.year"
+                        customClasses="w-full xl:w-1/2" required />
+
+                    <InputGroup label="Placa" type="text" placeholder="GAY2A24"
+                        customClasses="w-full xl:w-1/2" v-model="automobile.plate" required />
+                </div>
+
+                <InputGroup label="Modelo" type="text" placeholder="VW Pole" customClasses="mb-4.5"
+                v-model="automobile.model"  required />
+
+                <InputGroup label="Capacidade de passageiros" type="number" placeholder="4 viados" customClasses="mb-4.5" 
+                v-model="automobile.capacity" required />
+                <button
+                    class="flex w-full justify-center rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90">
+                    Cadastrar
+                </button>
+            </div>
+        </form>
+    </DefaultCard>
+</template>
--- src/views/Automoveis/index.vue
+++ src/views/Automoveis/index.vue
@@ -1,46 +1,27 @@
 <script setup lang="ts">
-import DataStatsOne from '@/components/DataStats/DataStatsOne.vue'
-import ChartOne from '@/components/Charts/ChartOne.vue'
-import ChartThree from '@/components/Charts/ChartThree.vue'
-import ChartTwo from '@/components/Charts/ChartTwo.vue'
-import ChatCard from '@/components/ChatCard.vue'
-import MapOne from '@/components/Maps/MapOne.vue'
-import TableOne from '@/components/Tables/TableOne.vue'
-import DefaultLayout from '@/layouts/DefaultLayout.vue'
+import AutomobileCard from '@/components/cards/AutomobileCard.vue';
+import DefaultLayout from '@/layouts/DefaultLayout.vue';
+import { reactive } from 'vue';
+
+const state = reactive({
+    automobile: {
+        year: null,
+        plate: '',
+        model: '',
+        capacity: null
+    }
+});
+
+const handleSubmit = async () => {
+    await console.log(state.automobile);
+}
+
 </script>
 
 <template>
     <DefaultLayout>
-        <div class="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-            <DataStatsOne />
-        </div>
-
-        <div class="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
-            <!-- ====== Chart One Start -->
-            <ChartOne />
-            <!-- ====== Chart One End -->
-
-            <!-- ====== Chart Two Start -->
-            <ChartTwo />
-            <!-- ====== Chart Two End -->
-
-            <!-- ====== Chart Three Start -->
-            <ChartThree />
-            <!-- ====== Chart Three End -->
-
-            <!-- ====== Map One Start -->
-            <MapOne />
-            <!-- ====== Map One End -->
-
-            <!-- ====== Table One Start -->
-            <div class="col-span-12 xl:col-span-8">
-                <TableOne />
-            </div>
-            <!-- ====== Table One End -->
-
-            <!-- ====== Chat Card Start -->
-            <ChatCard />
-            <!-- ====== Chat Card End -->
+        <div class="grid grid-cols-1 gap-4">
+            <AutomobileCard :automobile="state.automobile" title="Cadastrar automovel" @on-click:submit="handleSubmit"/>            
         </div>
     </DefaultLayout>
 </template>
